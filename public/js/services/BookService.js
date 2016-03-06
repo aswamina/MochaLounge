a44e04214bd35ef01e84a1d73b6e9fd592e3ce6d
Anand Swaminathan <aswamina@gmail.com>
2016-03-05 22:04:08 -0800

    added services for retrieving books and discounted books from MongoDB

--- public/js/services/BookService.js
+++ public/js/services/BookService.js
@@ -2,38 +2,25 @@
 
     var bookService = function ($http) {
 
-        var getUser = function (username) {
-            return $http.get("https://api.github.com/users/" + username).then(function (response) {
+        var getBooks = function () {
+            return $http.get('/getBooks').then(function (response) {
                 return response.data;
             });
         };
 
-        var getRepos = function (user) {
-            return $http.get(user.repos_url).then(function (response) {
+        var getBooksOnSale = function () {
+            return $http.get('/getBooksOnSale').then(function (response) {
                 return response.data;
-            });
-        };
-
-        var getRepoDetails = function (username, reponame) {
-            var repo;
-            var reposURL = "https://api.github.com/repos/" + username + "/" + reponame;
-            return $http.get(reposURL).then(function (response) {
-                repo = response.data;
-                return $http.get(reposURL + "/contributors");
-            }).then(function (response) {
-                repo.collaborators = response.data;
-                return repo;
             });
         };
 
         return {
-            getUser: getUser,
-            getRepos: getRepos,
-            getRepoDetails: getRepoDetails
+            getBooks: getBooks,
+            getBooksOnSale: getBooksOnSale
         };
     };
 
-    var module = angular.module("bookStore");
-    module.factory("BookService", bookService);
+    var module = angular.module('BookClub');
+    module.factory('BookService', bookService);
 
 }());
